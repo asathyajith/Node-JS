b16cea8f8e668a27f2ba18be411a05a2d34868f0
MOHANANA <AravindSathyajith.Mohanan@agilysys.com>
2023-01-10 11:11:24 +0530

    commit

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const PORT = 4000
+const app = express();
+const PORT = 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
